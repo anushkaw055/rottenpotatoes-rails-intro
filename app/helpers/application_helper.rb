5438e912d8194ccf3f3b652056f6762e6f630d39
EC2 Default User <ec2-user@ip-172-31-25-108.ec2.internal>
2020-09-16 02:40:19 +0000

    part 3 complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,3 +12,28 @@ module ApplicationHelper
 
 end
     
+def sort_helper(sort_column)
+        
+        #Set sort direction
+        sort_direction = params[:sort_direction]
+        if(sort_direction=="asc")
+            sort_direction="desc"
+        else
+            sort_direction = "asc"
+        end
+        
+        #Set Display string
+        link_display = ''
+        if(sort_column=='title')
+            link_display = 'Movie Title'
+        elsif(sort_column == 'release_date')
+            link_display = 'Release Date'
+        end
+        if(!@ratings_selected)
+            @ratings_selected = session[:ratings]
+        end
+        
+        link_to link_display, :sort_column => sort_column, :sort_direction => sort_direction, :ratings => @ratings_selected
+    end
+    
+end
